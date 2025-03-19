49dc9c93b1651e182aba3efcc7e9fec2a95c2e93
Victoria Gonzales <uzy7me@virginia.edu>
2025-03-18 20:22:22 -0400

    adding exercise files

---
+++ demo/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Demo',
+    version='0.1',
+    description='A simple demonstration package',
+    url='http://github.com/ontoligent/m09-demo',
+    author='R.C. Alvarado',
+    author_email='ontoligent@gmail.com',
+    license='MIT',
+    packages=['demo']
+)
